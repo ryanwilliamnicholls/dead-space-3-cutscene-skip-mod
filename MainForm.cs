d4156ee4a3a368095bf5570f8c4b1bee86176c37
Ryan Nicholls <ryanwilliamnicholls@gmail.com>
2021-05-24 19:58:25 +1000

    Adding solution for "double cutscene" bug where there are multiple small cutscenes in a single checkpoint. Keep track of the number of cutscenes seen in that checkpoint, and if it goes over a count you can skip the final cutscene in that checkpoint.

--- MainForm.cs
+++ MainForm.cs
@@ -16,12 +16,22 @@ namespace Flying47
         public string processName = "deadspace3";
 
         byte[] cpHex;
+        byte[] lastcpHex;
         Pointer checkpointHex = new Pointer("", 14605032, new int[0]);
         bool shouldSkip = true;
+        bool shouldIncrementCounter = true;
         int inCutsceneInt = 1;
         Pointer inCutscene= new Pointer("", 15176212, new int[] {304, 268, 260, 420, 352});
 
 
+        float readCoordX = 0;
+        float readCoordY = 0;
+        float readCoordZ = 0;
+
+        int cutscenesSeen = 0;
+
+        PositionSet_Pointer positionAddress = new PositionSet_Pointer(
+            new Pointer("", 0x00E10060, new int[] { 0x380, 0xB0 }), 4, true);
         /*------------------
         -- INITIALIZATION --
         ------------------*/
@@ -75,25 +85,61 @@ namespace Flying47
 
                     cpHex = Trainer.ReadPointerByteArray(myProcess, checkpointHex, 16);
                     inCutsceneInt = Trainer.ReadPointerInteger(myProcess, inCutscene);
-                    StringBuilder hex = new StringBuilder(cpHex.Length * 2);
-                    foreach (byte b in cpHex) { 
-                        hex.AppendFormat("{0:x2} ", b);
+                    positionAddress.ReadSet(myProcess, out readCoordX, out readCoordY, out readCoordZ);
+
+                    if (!cpHex[0].Equals(0x00) && readCoordX == 0 && readCoordY == 0 && readCoordZ == 0 && inCutsceneInt != 0)
+                    {
+                        //player has checkpoint reloaded when not in a cutscene, reset the counter for checkpoints they have seen in this checkpoint
+                        cutscenesSeen = 0;
                     }
 
-                    if(inCutsceneInt == 0 && !cpHex[0].Equals(0x00) && shouldSkip)
+                    if(cpHex[0].Equals(0x00))
+                    {
+                        shouldSkip = false; // dont skip cutscenes when loading to and from the main menu
+                    }
+
+                    if (inCutsceneInt == 0 && shouldSkip && CheckpointLookup.getCutsceneCountForCheckpoint(cpHex) == cutscenesSeen)
                     {
                         bool val = CheckpointLookup.swapCheckPointIfInTable(myProcess, checkpointHex, cpHex);
                         shouldSkip = !val;
+                        if (shouldSkip) 
+                        {
+                            // if the swap is called and does not swap, just increment the cutscene counter
+                            cutscenesSeen++;
+                        } else
+                        {
+                            // if the swap is called and does swap, stop the counter from incrementing til the checkpoint is reloaded
+                            shouldIncrementCounter = false;
+                        }
                     } else
                     {
                         if(inCutsceneInt != 0)
                         {
                             shouldSkip = true;
+                            shouldIncrementCounter = true;
+                        } else if (shouldIncrementCounter == true) // if your in a cutscene, and havent already incremented the counter for this cutscene, increment it
+                        {
+                            cutscenesSeen++;
+                            shouldIncrementCounter = false;
                         }
                     }
+
+                    if(!byteArrayEquals(cpHex, lastcpHex))
+                    {
+                        lastcpHex = cpHex;
+                        cutscenesSeen = 0;
+                    }
+
                     ShouldReload.Text = shouldSkip ? "Waiting for cutscene to skip" : "Checkpoint reload to skip this cutscene!";
                     ShouldReload.ForeColor = shouldSkip ? Color.Red : Color.Green;
+                    StringBuilder hex = new StringBuilder(cpHex.Length * 2);
+                    foreach (byte b in cpHex)
+                    {
+                        hex.AppendFormat("{0:x2} ", b);
+                    }
                     CheckpointVal.Text = hex.ToString();
+                   
+
                     InCutsceneBool.Text = inCutsceneInt == 0 ? "True" : "False";
                     TTimer.Interval = 100;
                 }
@@ -109,6 +155,15 @@ namespace Flying47
             {
                 Debug.WriteLine(ex.ToString());
             }
+        }
+
+        private bool byteArrayEquals(byte[] left, byte[] right)
+        {
+            if (left == null || right == null)
+            {
+                return left == right;
+            }
+            return left.SequenceEqual(right);
         }
 
         // Used to reset all the values.
